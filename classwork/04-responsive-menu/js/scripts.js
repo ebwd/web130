c7d727c653f8eb84d206e458574db2ef58b2e905
Erica Bechard <ebecha01@seattlecentral.edu>
2014-07-18 01:16:05 +0000

    added scripts for mobile responsive

--- classwork/04-responsive-menu/js/scripts.js
+++ classwork/04-responsive-menu/js/scripts.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
-  $("toggleMenu").css("display", "none");
-  
-  $(".nav li").hover(function() {
+  var ww = document.body.clientWidth;  
+  $(".toggleMenu").click(function(e) {
+    e.preventDefault();
+  $(".nav").toggle();
+   });
+  if (ww < 800) {
+    $(".toggleMenu").css("display", "inline-block");
+    $(".nav").hide();
+    $(".nav li a").click(function() {
+      $(this).parent("li").toggleClass("hover");
+    });
+  } else {
+    $("toggleMenu").css("display", "none");
+    $(".nav li").hover(function() {
     $(this).addClass("hover");
-  }, function(){
+    }, function(){
     $(this).removeClass("hover");
-  }); 
-});
+  });
+  }
+
+  $(".nav li a").each (function(){
+    if ( $(this).next().length > 0 ) {
+      $(this).addClass("parent");
+    }
+  });
+
+});//end document.ready function
+
+
+
+
